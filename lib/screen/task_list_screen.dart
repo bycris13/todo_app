ad6852a1ecb7937edd0ad01edfe9e9311892ef5d
Cristian <cristianariasu13@gmail.com>
2025-07-02 19:23:25 -0500

    :art: Caja de texto y boton de agregar tarea

--- lib/screen/task_list_screen.dart
+++ lib/screen/task_list_screen.dart
@@ -9,50 +9,97 @@ class TaskListScreen extends StatefulWidget {
 }
 
 class _TaskListScreenState extends State<TaskListScreen> {
-  final List<Task> tasks = [
-    Task(title: 'Aprender Flutter POO', completed: false),
-    Task(title: 'Practicar Provider', completed: false),
-    Task(title: 'Crear app de tareas', completed: false),
-  ];
+  final List<Task> tasks = [];
+
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Mi primera App de Tareas POO'),
+          title: const Text('App de Tareas'),
           centerTitle: true,
         ),
-        body: ListView.builder(
-          itemCount: tasks.length,
-          itemBuilder: (context, index) {
-            final task = tasks[index];
-            return Card(
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-              child: ListTile(
-                leading: Checkbox(
-                    activeColor: Colors.green,
-                    value: task.completed,
-                    onChanged: (bool? value) {
-                      setState(
-                        () {
-                          task.toggleCompleted();
-                        },
-                      );
-                    }),
-                title: Text(
-                  task.title,
-                  style: TextStyle(
-                      fontSize: 20,
-                      decoration: task.completed
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none),
-                ),
-                trailing: IconButton(
-                    onPressed: () {}, icon: const Icon(Icons.delete)),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        labelText: 'Escribe una tarea',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                      onPressed: () {
+                        final String text = _controller.text.trim();
+                        if (text.isNotEmpty) {
+                          setState(() {
+                            tasks.add(Task(title: text, completed: false));
+                            _controller.clear();
+                          });
+                        }
+                      },
+                      child: const Text('Agregar')),
+                ],
               ),
-            );
-          },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  final task = tasks[index];
+                  return Card(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 9.0),
+                    child: ListTile(
+                      leading: Checkbox(
+                          activeColor: Colors.green,
+                          value: task.completed,
+                          onChanged: (bool? value) {
+                            setState(
+                              () {
+                                task.toggleCompleted();
+                              },
+                            );
+                          }),
+                      title: Text(
+                        task.title,
+                        style: TextStyle(
+                            fontSize: 20,
+                            decoration: task.completed
+                                ? TextDecoration.lineThrough
+                                : TextDecoration.none),
+                      ),
+                      trailing: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              tasks.removeAt(index);
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          )),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ));
   }
 }
